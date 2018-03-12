c249173874a8a7e2fb19307f4a0d080ce0dbdfd4
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-12 16:41:28 +0530

    string.c
    good

---
+++ string.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+using namespace std;
+ 
+int count(string s, int K)
+{
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+     {
+     c=c+;
+        }
+}
+    return C * K + (K * (K - 1) / 2) * c1 * c2;
+}
+int main()
+{
+    string S = "abcb";
+    int k = 2;
+    cout << countOccurrences(S, k) << endl; 
+    return 0;
+}
